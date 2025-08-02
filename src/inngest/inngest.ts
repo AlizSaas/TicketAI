0001bb2f68e5622efb20798cea6fc094b16b2587
zxzAli <blackbearddevss@gmail.com>
2025-08-02 18:29:17 -0400

    eventkey

--- src/inngest/inngest.ts
+++ src/inngest/inngest.ts
@@ -6,7 +6,9 @@ import { Inngest } from 'inngest';
 export const inngest = new Inngest({
   id: 'ticketing-system',
   name: 'Ticketing System Inngest',
-  signingKey: process.env.INNGEST_SIGNING_KEY!
+  signingKey: process.env.INNGEST_SIGNING_KEY!,
+  eventKey: process.env.INNGEST_EVENT_KEY!,
+
 
 
 });
